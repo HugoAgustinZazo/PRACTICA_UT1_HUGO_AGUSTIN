ddfeded262e9aa1e91b5901a198e8df63abe5789
Hugo <huguitronic17@gmail.com>
2024-10-14 21:36:12 +0200

    ultimas modificaciones

--- src/main/java/org/example/GestionLog.java
+++ src/main/java/org/example/GestionLog.java
@@ -1,6 +1,21 @@
 package org.example;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,100 +24,99 @@ import java.util.*;
 public class GestionLog {
     static String filtro;
     static ArrayList<LineaLog> lineas = new ArrayList<>();
-    public static void leerLog(){
-        try{
+
+    public static void leerLog() {
+        try {
             Path path = Paths.get("src/log_ejemplo.log");
-             List<String> l = Files.readAllLines(path);
-        String[] partes;
+            List<String> l = Files.readAllLines(path);
+            String[] partes;
 
-        for(String linea:l){
-            partes=linea.split(" ");
+            for (String linea : l) {
+                partes = linea.split(" ");
 
-            String fecha = partes[0]+" "+partes[1];
-            String nivel = partes[2];
-            String mensaje="";
-            for(int i=3;i< partes.length;i++) {
-                mensaje =mensaje + (partes[i] + " ");
+                String fecha = partes[0] + " " + partes[1];
+                String nivel = partes[2];
+                String mensaje = "";
+                for (int i = 3; i < partes.length; i++) {
+                    mensaje = mensaje + (partes[i] + " ");
+                }
+                lineas.add(new LineaLog(fecha, nivel, mensaje));
             }
-            lineas.add(new LineaLog(fecha,nivel,mensaje));
-        }
-        String[] quitarCorchetes;
-        String[] quitarCorchetes2;
-            for(LineaLog lin: lineas){
-            quitarCorchetes= lin.getFecha().split("");
-            String fecha="";
-            for(int i=1;i< quitarCorchetes.length-1;i++){
-                fecha=fecha+quitarCorchetes[i];
+            String[] quitarCorchetes;
+            String[] quitarCorchetes2;
+            for (LineaLog lin : lineas) {
+                quitarCorchetes = lin.getFecha().split("");
+                String fecha = "";
+                for (int i = 1; i < quitarCorchetes.length - 1; i++) {
+                    fecha = fecha + quitarCorchetes[i];
+                }
+                quitarCorchetes2 = lin.getNivel().split("");
+                String nivel = "";
+                for (int j = 1; j < quitarCorchetes2.length - 1; j++) {
+                    nivel = nivel + quitarCorchetes2[j];
+                }
+                lin.setFecha(fecha);
+                lin.setNivel(nivel);
             }
-            quitarCorchetes2= lin.getNivel().split("");
-            String nivel="";
-            for(int j=1;j< quitarCorchetes2.length-1;j++){
-                nivel=nivel+quitarCorchetes2[j];
-            }
-               lin.setFecha(fecha);
-               lin.setNivel(nivel);
-        }
-            for(LineaLog lin: lineas){
-                System.out.println(lin);
-            }
-
+            System.out.println("\nLOG CARGADO CORRECTAMENTE");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
     }
-    public static void elegirNivel(){
+
+    public static void elegirNivel() {
         Scanner teclado = new Scanner(System.in);
         boolean salir = false;
 
-        while(!salir) {
+        while (!salir) {
             try {
                 menuNiveles();
                 int nivelelegido = teclado.nextInt();
 
 
-                    switch (nivelelegido){
-                        case 1:
-                            for(LineaLog lin: lineas){
+                switch (nivelelegido) {
+                    case 1:
+                        for (LineaLog lin : lineas) {
+                            System.out.println(lin);
+                        }
+                        filtro = "NINGUNO";
+                        salir = true;
+                        break;
+                    case 2:
+                        for (LineaLog lin : lineas) {
+                            if (lin.getNivel().equalsIgnoreCase("INFO")) {
                                 System.out.println(lin);
                             }
-                            filtro="NINGUNO";
-                            salir = true;
-                            break;
-                        case 2:
-                            for(LineaLog lin: lineas){
-                               if(lin.getNivel().equalsIgnoreCase("INFO")) {
-                                   System.out.println(lin);
-                               }
+                        }
+                        filtro = "INFO";
+                        salir = true;
+                        break;
+                    case 3:
+                        for (LineaLog lin : lineas) {
+                            if (lin.getNivel().equalsIgnoreCase("WARNING")) {
+                                System.out.println(lin);
                             }
-                            filtro="INFO";
-                            salir = true;
-                            break;
-                        case 3:
-                            for(LineaLog lin: lineas){
-                                if(lin.getNivel().equalsIgnoreCase("WARNING")) {
-                                    System.out.println(lin);
-                                }
+                        }
+                        filtro = "WARNING";
+                        salir = true;
+                        break;
+                    case 4:
+                        for (LineaLog lin : lineas) {
+                            if (lin.getNivel().equalsIgnoreCase("ERROR")) {
+                                System.out.println(lin);
                             }
-                            filtro="WARNING";
-                            salir = true;
-                            break;
-                        case 4:
-                            for(LineaLog lin: lineas){
-                                if(lin.getNivel().equalsIgnoreCase("ERROR")) {
-                                    System.out.println(lin);
-                                }
-                            }
-                            filtro="ERROR";
-                            salir = true;
-                            break;
-                        case 5:
-                            salir = true;
-                            break;
-                        default:
-                            System.out.println("El numero escogido no corresponde con ninguna opcion");
-                            break;
-                    }
+                        }
+                        filtro = "ERROR";
+                        salir = true;
+                        break;
+                    case 5:
+                        salir = true;
+                        break;
+                    default:
+                        System.out.println("El numero escogido no corresponde con ninguna opcion");
+                        break;
+                }
             } catch (InputMismatchException e) {
                 System.out.println("El dato introducido no es correcto debes introducir un número\n");
                 teclado.nextLine();
@@ -110,8 +124,68 @@ public class GestionLog {
         }
     }
 
-    public static void menuNiveles(){
+    public static void menuNiveles() {
         System.out.println("Elige el nivel por el que quieras filtrar\n1-NINGUNO(muestra todos)\n2-INFO\n3-WARNING\n4-ERROR\n5-SALIR");
 
+    }
+
+    public static void volcarLog(String filtro, String ficheroSalida) throws ParserConfigurationException, TransformerException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.newDocument();
+
+        Element elementoPrincipal = doc.createElement("logAplicacion");
+        doc.appendChild(elementoPrincipal);
+
+        Element log;
+
+        for (LineaLog linea : lineas) {
+            if (linea.getNivel().equalsIgnoreCase(filtro)) {
+
+                log = doc.createElement("log");
+                elementoPrincipal.appendChild(log);
+
+                Element fechaHora = doc.createElement("fechaHora");
+                fechaHora.appendChild(doc.createTextNode(linea.getFecha()));
+                log.appendChild(fechaHora);
+
+                Element nivel = doc.createElement("nivel");
+                nivel.appendChild(doc.createTextNode(linea.getNivel()));
+                log.appendChild(nivel);
+
+
+                Element mensaje = doc.createElement("mensaje");
+                mensaje.appendChild(doc.createTextNode(linea.getMensaje()));
+                log.appendChild(mensaje);
+
+            }
+
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        DOMSource source = new DOMSource(doc);
+
+        StreamResult result = new StreamResult(new File(ficheroSalida));
+        transformer.transform(source, result);
+
+    }
+
+    public static void volcarLogJson(String filtro, String ficheroSalida) throws ParserConfigurationException, TransformerException, IOException {
+       /* try (JsonWriter writer = new JsonWriter(new FileWriter("json/" + ficheroSalida + ".json"))) {
+            writer.setIndent("  ");
+            writer.beginArray();
+
+            for (LineaLog linea : lineas) {
+                writePersona(writer, linea);
+            }
+            writer.endArray();
+            System.out.println("Se ha creado el fichero JSON correctamente usando streams.");
+
+
+        }
+
+        */
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,15 @@
 package org.example;
 
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-import static org.example.GestionLog.elegirNivel;
-import static org.example.GestionLog.leerLog;
 
 
 public class Main {
     public static void main(String[] args) {
-            leerLog();
-            elegirNivel();
         Scanner teclado = new Scanner(System.in);
         boolean salir = false;
         while(!salir) {
@@ -21,21 +20,28 @@ public class Main {
 
                 switch (opcion){
                     case 1:
-                        leerLog();
+                        GestionLog.leerLog();
                         break;
                     case 2:
-                        elegirNivel();
+                        GestionLog.elegirNivel();
 
                         break;
                     case 3:
-
+                        System.out.println("Dime un nombre para el archivo xml(el .xml no hace falta que lo pongas)");
+                        String archivoxml = teclado.next();
+                        GestionLog.volcarLog(GestionLog.filtro,"src/"+archivoxml+".xml");
                         break;
                     case 4:
-
+                        System.out.println("Dime un nombre para el archivo xml(el .xml no hace falta que lo pongas)");
+                        String archivojson= teclado.next();
+                        GestionLog.volcarLogJson(GestionLog.filtro, archivojson);
                         break;
                     case 5:
-                        salir = true;
+
                         break;
+                        case 6:
+                            salir = true;
+                            break;
                     default:
                         System.out.println("El numero escogido no corresponde con ninguna opcion");
                         break;
@@ -43,11 +49,17 @@ public class Main {
             } catch (InputMismatchException e) {
                 System.out.println("El dato introducido no es correcto debes introducir un número\n");
                 teclado.nextLine();
+            } catch (ParserConfigurationException e) {
+                throw new RuntimeException(e);
+            } catch (TransformerException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
         }
         }
     public static void menu(){
-        System.out.println("\n1-Carga del archivo .log y carga de trazas en memoria. \n2-Filtrado de trazas por nivel de log.\n3-Exportación de logs filtrados a XML.\n4-Exportación de logs filtrados a JSON.\n5-Resetear filtrado (quita el filtrado previo)\n");
+        System.out.println("\n1-Carga del archivo .log y carga de trazas en memoria. \n2-Filtrado de trazas por nivel de log.\n3-Exportación de logs filtrados a XML.\n4-Exportación de logs filtrados a JSON.\n5-Resetear filtrado (quita el filtrado previo)\n6-SALIR DEL PROGRAMA");
 
     }
     }
